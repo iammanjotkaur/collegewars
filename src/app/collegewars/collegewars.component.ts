f71fff3d81d4ab1092081ad7ed4296107e75239a
manjot kaur <manjot.kaur97@gmail.com>
2020-02-12 01:05:50 +0530

    initial commit

---
+++ src/app/collegewars/collegewars.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collegewars',
+  templateUrl: './collegewars.component.html',
+  styleUrls: ['./collegewars.component.css']
+})
+export class CollegewarsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
